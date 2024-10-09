17e5caaac45a37a89be6e18cc85ed39622133b44
rivenuser2 <cmj3905@naver.com>
2024-10-09 10:58:05 +0900

    첫번쨰 시도 구상위주
    거의 구상위주로함

---
+++ test1.java
@@ -0,0 +1,20 @@
+package makingcomputer01;
+
+import javax.swing.JFrame;
+
+public class test1 extends JFrame {
+
+	test1() {
+		setTitle("계산기");
+		// 제목은 계산기
+		// 결과를 16,10,8,2진수로 4가지 출력예쩡
+		// 0~9까지 숫자,+-구분자,소수점,+,-,/,*,c,log10^x,x^2,=,문자한개삭제,펙토리얼,괄호(,),이렇게 해서 버튼24개 그리고
+		// 결과화면5개
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
